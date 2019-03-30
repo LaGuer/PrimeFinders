cc3604bf9ce0a869f1f9d4da0c17dfb58694482a
Laurent G. OSSBSS AMIS UCC <27735554+LaGuer@users.noreply.github.com>
2019-03-30 03:30:36 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -8,7 +8,6 @@ from primefinders import primefinders
 # or an object inside the primefinders module
 from primefinders.primefinders import fermat
 
-import unittest
 import tests.all_tests
 testSuite = tests.all_tests.create_test_suite()
 text_runner = unittest.TextTestRunner().run(testSuite)
